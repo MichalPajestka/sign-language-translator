fb8102785673a914043414b2beb6aef13bb0252f
MichalPajestka <001michal002@gmail.com>
2023-09-20 12:46:48 +0200

    Add a temp delete translation function

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,6 +1,13 @@
+import PNavbar from "../components/Profile/ProfileNavbar";
+import { deleteTranslations } from "../utils/deleteTranslations"; // Import the function correctly
+
 const Profile = () => {
+
     return (
-        <h1>Profile</h1>
+        <div>
+            <PNavbar></PNavbar>
+            <button onClick={deleteTranslations}>Delete Translations</button>
+        </div>
     )
 }
 export default Profile;
