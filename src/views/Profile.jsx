a2a872ff260bb617c243963f2aaef5bf9dc0a7c6
MichalPajestka <001michal002@gmail.com>
2023-09-20 12:53:53 +0200

    Remove unused functions and components -Caused conflicts

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,13 +1,6 @@
-import PNavbar from "../components/Profile/ProfileNavbar";
-import { deleteTranslations } from "../utils/deleteTranslations"; // Import the function correctly
-
 const Profile = () => {
-
     return (
-        <div>
-            <PNavbar></PNavbar>
-            <button onClick={deleteTranslations}>Delete Translations</button>
-        </div>
+        <h1>Profile</h1>
     )
 }
 export default Profile;
