b6fd129eb7f4e237f3d623b202f5374ab050b5f2
MichalPajestka <001michal002@gmail.com>
2023-09-20 11:20:49 +0200

    Fix the Translation function -Display message when input has reached 40 chars -Input must contain at least one A to Z character -Make the translation ignore special chars.

--- src/components/Translation/TranslationInput.jsx
+++ src/components/Translation/TranslationInput.jsx
@@ -1,58 +1,73 @@
 import { useForm } from "react-hook-form";
+import { useState } from "react";
 import translate from "../../utils/translate";
 
-
 const TranslationInput = ({ updateTranslatedText }) => {
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const { register, handleSubmit } = useForm();
+  const [inputValue, setInputValue] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isTranslateButtonDisabled, setIsTranslateButtonDisabled] = useState(true);
+
+  const handleInputChange = (e) => {
+    const cleanedText = e.target.value;
+    setInputValue(cleanedText);
+
+    if (cleanedText.length === 40) {
+      setErrorMessage("Character limit reached (max: 40)");
+    } else if (!/[a-zA-Z]/.test(cleanedText)) {
+      setErrorMessage("Please enter at least one alphabetic character (a to z)");
+      setIsTranslateButtonDisabled(true); // Disable the button
+    } else {
+      setErrorMessage("");
+      setIsTranslateButtonDisabled(false); // Enable the button
+    }
+  };
 
   const onSubmit = async (data) => {
     try {
-       // Check if the input is empty or contains non-letter characters
-      if (!/^[a-zA-Z]+$/.test(data.translation)) {
-        console.error("Invalid input. Please enter only letters.");
-        return; // Don't proceed with translation
-      }
+      const cleanedText = data.translation;
 
-      const translation = translate(data.translation);
+      const translation = translate(cleanedText);
       updateTranslatedText(translation);
 
       const response = await fetch(
-          "https://translations-api-production-3e9d.up.railway.app/translations",
-          {
-            method: "POST",
-            headers: {
-              'X-API-Key': 'xqW942yHAcoehSs1JRI9pMbAqTNIGl0hFEIdLgvS6cgogVlCrWzn7bWMIULvxQ3o',
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-            translations: [data.translation],
+        "https://translations-api-production-3e9d.up.railway.app/translations",
+        {
+          method: "POST",
+          headers: {
+            'X-API-Key': 'xqW942yHAcoehSs1JRI9pMbAqTNIGl0hFEIdLgvS6cgogVlCrWzn7bWMIULvxQ3o',
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            translations: [cleanedText],
           }),
         }
-        );
-        
-        if (!response.ok) {
-          throw new Error("Translation request failed");
-        }
+      );
 
-        // Clear the input field after successful submission
-        
-        } catch (error) {
-            console.error('Translation error:', error);
-        }
-    };
+      if (!response.ok) {
+        throw new Error("Translation request failed");
+      }
+
+      setIsTranslateButtonDisabled(true); // Disable the button again after successful submission
+
+      setErrorMessage("");
+    } catch (error) {
+      console.error('Translation error:', error);
+    }
+  };
 
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
         <input
-          {...register("translation", {
-            maxLength: 40,
-          })}
+          {...register("translation")}
           type="text"
+          maxLength="40"
+          value={inputValue}
+          onChange={handleInputChange}
         />
-
-        {errors.translation && <p>The text you input is too long! (max: 40 characters)</p>}
-        <button type="submit">Translate</button>
+        <button type="submit" disabled={isTranslateButtonDisabled}>Translate</button>
+        {errorMessage && <p>{errorMessage}</p>}
       </form>
     </div>
   );
--- src/utils/translate.js
+++ src/utils/translate.js
@@ -1,59 +1,59 @@
+import React from 'react';
+
 const images = require.context('../resources/individial_signs/', false, /\.(png)$/);
 
-//Map the letters to sign language images
+// Map the letters to sign language images
 const letterToSignImage = {
-    a: 'a.png',
-    b: 'b.png',
-    c: 'c.png',
-    d: 'd.png',
-    e: 'e.png',
-    f: 'f.png',
-    g: 'g.png',
-    h: 'h.png',
-    i: 'i.png',
-    j: 'j.png',
-    k: 'k.png',
-    l: 'l.png',
-    m: 'm.png',
-    n: 'n.png',
-    o: 'o.png',
-    p: 'p.png',
-    q: 'q.png',
-    r: 'r.png',
-    s: 's.png',
-    t: 't.png',
-    u: 'u.png',
-    v: 'v.png',
-    w: 'w.png',
-    x: 'x.png',
-    y: 'y.png',
-    z: 'z.png',
-  };
+  a: 'a.png',
+  b: 'b.png',
+  c: 'c.png',
+  d: 'd.png',
+  e: 'e.png',
+  f: 'f.png',
+  g: 'g.png',
+  h: 'h.png',
+  i: 'i.png',
+  j: 'j.png',
+  k: 'k.png',
+  l: 'l.png',
+  m: 'm.png',
+  n: 'n.png',
+  o: 'o.png',
+  p: 'p.png',
+  q: 'q.png',
+  r: 'r.png',
+  s: 's.png',
+  t: 't.png',
+  u: 'u.png',
+  v: 'v.png',
+  w: 'w.png',
+  x: 'x.png',
+  y: 'y.png',
+  z: 'z.png',
+};
 
-  //Translate a word or sentance to sign language
-  function translate(text) {
-    const lowerText = text.toLowerCase();
+// Translate a word or sentence to sign language
+function translate(text) {
+  const lowerText = text.toLowerCase();
 
-    const letters = lowerText.split('');
+  // Filter out non-alphabetic characters
+  const letters = lowerText.split('').filter((letter) => /[a-z]/.test(letter));
 
-    const translation = letters.map((letter, index) => {
-        //Check if letter has corresponding image
-        if (letterToSignImage.hasOwnProperty(letter)) {
-            const imageFilename = letterToSignImage[letter];
+  const translation = letters.map((letter, index) => {
+    // Check if letter has a corresponding image
+    if (letterToSignImage.hasOwnProperty(letter)) {
+      const imageFilename = letterToSignImage[letter];
+      const imgSrc = images(`./${imageFilename}`);
 
-            const imgSrc = images(`./${imageFilename}`);
+      return <img key={index} src={imgSrc} alt={letter} />;
+    } else {
+      return letter;
+    }
+  });
 
-            return (
-                <img key={index} src={imgSrc} alt={letter} /> 
-            )
-        } else {
-            return letter;  
-        }
-    });
+  translation.unshift(<p key="heading">Translated Text:</p>);
 
-    translation.unshift(<p key="heading">Translated Text:</p>);
+  return translation;
+}
 
-    return translation;
-  }
-  export default translate;
-  
+export default translate;
