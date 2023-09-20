77711f5332fb53dbbc22654ea4fc1553c85f859f
MichalPajestka <001michal002@gmail.com>
2023-09-20 12:34:41 +0200

    Create Navbar -Created a simple navbar for the profile page

---
+++ src/components/Profile/ProfileNavbar.jsx
@@ -0,0 +1,11 @@
+import { NavLink } from "react-router-dom"
+
+const PNavbar = () => {
+    return(
+        <div>
+            <NavLink to="/translation"><h2>Lost in Translation</h2></NavLink>
+            <NavLink to="/">Log out</NavLink>
+        </div>
+    )
+}
+export default PNavbar
