14537ade942ce4f3fe09bfc041e5dcca1fea33ce
MichalPajestka <001michal002@gmail.com>
2023-09-20 12:50:49 +0200

    Add navbar to profile page

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,6 +1,11 @@
+import PNavbar from "../components/Profile/ProfileNavbar";
+
 const Profile = () => {
     return (
-        <h1>Profile</h1>
+        <div>
+            <PNavbar></PNavbar>
+            <h1>Profile</h1>
+        </div>
     )
 }
 export default Profile;
