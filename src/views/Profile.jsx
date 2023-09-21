11b411b9b5f39bd34f18f6dfe12a9e7dd040c38d
MichalPajestka <001michal002@gmail.com>
2023-09-21 10:18:55 +0200

    Add check for if user is logged in

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,7 +1,23 @@
 import PNavbar from "../components/Profile/ProfileNavbar";
 import { deleteTranslations } from "../utils/deleteTranslations";
 
+import { useNavigate } from "react-router-dom";
+import React, { useEffect } from "react";
+
 const Profile = () => {
+
+    /*const navigate = useNavigate();
+
+    useEffect(() => {
+      // Check if the user is logged in
+      const isLoggedIn = localStorage.getItem("isLoggedIn");
+  
+      if (!isLoggedIn || isLoggedIn !== "true") {
+        // If not logged in, redirect to the login page
+        navigate("/");
+      }
+    }, [navigate]); */
+    
     return (
         <div>
             <PNavbar></PNavbar>
