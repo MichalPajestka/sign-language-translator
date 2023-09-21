7f24c695cb044e760722927ff1ef5358b7b13e36
MichalPajestka <001michal002@gmail.com>
2023-09-21 09:27:04 +0200

    Change <h2> tag value

--- src/components/Translation/TranlsationNavbar.jsx
+++ src/components/Translation/TranlsationNavbar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom"
 const TNavbar = () => {
     return(
         <div>
-            <h2>Lost in Translation</h2>
+            <h2>Sign Language Translator</h2>
             <NavLink to="/profile">Profile</NavLink>
         </div>
     )
