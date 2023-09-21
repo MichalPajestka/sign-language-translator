d767b6aabdf9a8de6fb3d7809b29c26a85a08459
OzKara <oz_247@hotmail.com>
2023-09-22 01:01:36 +0200

    Implemented ClearTranslations component

--- src/components/Profile/ClearTranslations.jsx
+++ src/components/Profile/ClearTranslations.jsx
@@ -1,9 +1,38 @@
 import React from "react";
 
-const ClearTranslations = ({ clearTranslations }) => {
+const ClearTranslations = () => {
+	const username = localStorage.getItem("username");
+
+	const handleClearTranslations = async () => {
+		try {
+			const response = await fetch(
+				`https://translations-api-production-3e9d.up.railway.app/translations?username=${username}`,
+				{
+					method: "DELETE",
+					headers: {
+						"X-API-Key":
+							"xqW942yHAcoehSs1JRI9pMbAqTNIGl0hFEIdLgvS6cgogVlCrWzn7bWMIULvxQ3o",
+						"Content-Type": "application/json",
+					},
+				}
+			);
+
+			if (!response.ok) {
+				throw new Error("Failed to clear translations");
+			}
+
+			// Clear the translations in the component state or perform any necessary updates
+			console.log("Translations cleared successfully");
+		} catch (error) {
+			console.error("Error clearing translations:", error);
+		}
+	};
+
 	return (
 		<div>
-			<button onClick={clearTranslations}>Clear Translations</button>
+			<button onClick={handleClearTranslations}>
+				Clear Translations
+			</button>
 		</div>
 	);
 };
