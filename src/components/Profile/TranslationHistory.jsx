d7e625b80da38a5d05a6bcf1a6bb5b75dfe60173
OzKara <oz_247@hotmail.com>
2023-09-22 01:01:15 +0200

    Implemented TranslationHistory component

--- src/components/Profile/TranslationHistory.jsx
+++ src/components/Profile/TranslationHistory.jsx
@@ -1,6 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const TranslationHistory = ({ translations }) => {
+const TranslationHistory = () => {
+	const [translations, setTranslations] = useState([]);
+	const username = localStorage.getItem("username");
+
+	useEffect(() => {
+		const fetchTranslations = async () => {
+			try {
+				const response = await fetch(
+					`https://translations-api-production-3e9d.up.railway.app/translations?username=${username}`,
+					{
+						method: "GET",
+						headers: {
+							"X-API-Key":
+								"xqW942yHAcoehSs1JRI9pMbAqTNIGl0hFEIdLgvS6cgogVlCrWzn7bWMIULvxQ3o",
+							"Content-Type": "application/json",
+						},
+					}
+				);
+
+				if (!response.ok) {
+					throw new Error("Failed to fetch translation history");
+				}
+
+				const data = await response.json();
+				setTranslations(data[0]?.translations || []);
+			} catch (error) {
+				console.error("Error fetching translation history:", error);
+			}
+		};
+
+		fetchTranslations();
+	}, [username]);
+
 	return (
 		<div>
 			<h3>Your Last 10 Translations</h3>
