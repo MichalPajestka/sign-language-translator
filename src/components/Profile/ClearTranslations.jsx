fa9fda42a4672b3a7499bcb25acc75c218cd9d9c
MichalPajestka <001michal002@gmail.com>
2023-09-22 15:12:02 +0200

    fix clearTranslation

--- src/components/Profile/ClearTranslations.jsx
+++ src/components/Profile/ClearTranslations.jsx
@@ -1,40 +1,102 @@
-import React from "react";
-
 const ClearTranslations = () => {
-	const username = localStorage.getItem("username");
+  const username = localStorage.getItem("username");
+  let userId; // Define userId here
 
-	const handleClearTranslations = async () => {
-		try {
-			const response = await fetch(
-				`https://translations-api-production-3e9d.up.railway.app/translations?username=${username}`,
-				{
-					method: "DELETE",
-					headers: {
-						"X-API-Key":
-							"xqW942yHAcoehSs1JRI9pMbAqTNIGl0hFEIdLgvS6cgogVlCrWzn7bWMIULvxQ3o",
-						"Content-Type": "application/json",
-					},
-				}
-			);
+  const fetchTranslations = async () => {
+    try {
+      const response = await fetch(
+        `https://translations-api-production-3e9d.up.railway.app/translations?username=${username}`,
+        {
+          method: "GET",
+          headers: {
+            "X-API-Key":
+              "xqW942yHAcoehSs1JRI9pMbAqTNIGl0hFEIdLgvS6cgogVlCrWzn7bWMIULvxQ3o",
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
-			if (!response.ok) {
-				throw new Error("Failed to clear translations");
-			}
+      if (!response.ok) {
+        throw new Error("Failed to fetch user data");
+      }
 
-			// Clear the translations in the component state or perform any necessary updates
-			console.log("Translations cleared successfully");
-		} catch (error) {
-			console.error("Error clearing translations:", error);
-		}
-	};
+      const userData = await response.json();
 
-	return (
-		<div>
-			<button onClick={handleClearTranslations}>
-				Clear Translations
-			</button>
-		</div>
-	);
+      // Check if user data is in the expected format
+      if (Array.isArray(userData) && userData.length > 0) {
+        // Extract the user's ID
+        userId = userData[0].id; // Assign the value to userId
+        // ...
+      } else {
+        console.error("User data is not in the expected format:", userData);
+      }
+    } catch (error) {
+      console.error("Error clearing translations:", error);
+    }
+  };
+
+  const handleClearTranslations = async () => {
+    try {
+      // Fetch the user's data, including translations
+      const response = await fetch(
+        `https://translations-api-production-3e9d.up.railway.app/translations?username=${username}`,
+        {
+          method: "GET",
+          headers: {
+            "X-API-Key":
+              "xqW942yHAcoehSs1JRI9pMbAqTNIGl0hFEIdLgvS6cgogVlCrWzn7bWMIULvxQ3o",
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch user data");
+      }
+
+      const userData = await response.json();
+
+      // Check if user data is in the expected format
+      if (Array.isArray(userData) && userData.length > 0) {
+        // Extract the user's ID
+        userId = userData[0].id; // Assign the value to userId
+
+        // Clear the user's translations by sending a PATCH request with an empty array of translations
+        const clearResponse = await fetch(
+          `https://translations-api-production-3e9d.up.railway.app/translations/${userId}`,
+          {
+            method: "PATCH",
+            headers: {
+              "X-API-Key":
+                "xqW942yHAcoehSs1JRI9pMbAqTNIGl0hFEIdLgvS6cgogVlCrWzn7bWMIULvxQ3o",
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ translations: [] }),
+          }
+        );
+
+        if (!clearResponse.ok) {
+          throw new Error("Failed to clear translations");
+        }
+
+        // Call the fetchTranslations function to update the translation history
+        fetchTranslations();
+
+        // Clear the translations in the component state or perform any necessary updates
+        console.log("Translations cleared successfully");
+      } else {
+        console.error("User data is not in the expected format:", userData);
+      }
+    } catch (error) {
+      console.error("Error clearing translations:", error);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleClearTranslations}>Clear Translations</button>
+    </div>
+  );
 };
 
 export default ClearTranslations;
--- src/components/Profile/TranslationHistory.jsx
+++ src/components/Profile/TranslationHistory.jsx
@@ -1,53 +1,52 @@
 import React, { useEffect, useState } from "react";
 
 const TranslationHistory = () => {
-	const [translations, setTranslations] = useState([]);
-	const username = localStorage.getItem("username");
+  const [translations, setTranslations] = useState([]);
+  const username = localStorage.getItem("username");
 
-	useEffect(() => {
-		const fetchTranslations = async () => {
-		  try {
-			const response = await fetch(
-			  `https://translations-api-production-3e9d.up.railway.app/translations?username=${username}`,
-			  {
-				method: "GET",
-				headers: {
-				  "X-API-Key":
-					"xqW942yHAcoehSs1JRI9pMbAqTNIGl0hFEIdLgvS6cgogVlCrWzn7bWMIULvxQ3o",
-				  "Content-Type": "application/json",
-				},
-			  }
-			);
-	  
-			if (!response.ok) {
-			  throw new Error("Failed to fetch translation history");
-			}
-	  
-			const data = await response.json();
-	  
-			// Reverse the order of translations before setting the state
-			const lastTenTranslations = data[0]?.translations.reverse() || [];
-			setTranslations(lastTenTranslations);
-		  } catch (error) {
-			console.error("Error fetching translation history:", error);
-		  }
-		};
-	  
-		fetchTranslations();
-	  }, [username]);
-	  
+  const fetchTranslations = async () => {
+    try {
+      const response = await fetch(
+        `https://translations-api-production-3e9d.up.railway.app/translations?username=${username}`,
+        {
+          method: "GET",
+          headers: {
+            "X-API-Key":
+              "xqW942yHAcoehSs1JRI9pMbAqTNIGl0hFEIdLgvS6cgogVlCrWzn7bWMIULvxQ3o",
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
-	return (
-		<div>
-			<h3>Your Last 10 Translations</h3>
-			<ul>
-				{translations.slice(0, 10).map((translation, index) => (
-					<li key={index}>{translation}</li>
-				))}
-			</ul>
-			{translations.length === 0 && <p>No translations available.</p>}
-		</div>
-	);
+      if (!response.ok) {
+        throw new Error("Failed to fetch translation history");
+      }
+
+      const data = await response.json();
+
+      // Reverse the order of translations before setting the state
+      const lastTenTranslations = data[0]?.translations.reverse() || [];
+      setTranslations(lastTenTranslations);
+    } catch (error) {
+      console.error("Error fetching translation history:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTranslations();
+  }, [username, fetchTranslations]);
+
+  return (
+    <div>
+      <h3>Your Last 10 Translations</h3>
+      <ul>
+        {translations.slice(0, 10).map((translation, index) => (
+          <li key={index}>{translation}</li>
+        ))}
+      </ul>
+      {translations.length === 0 && <p>No translations available.</p>}
+    </div>
+  );
 };
 
 export default TranslationHistory;
