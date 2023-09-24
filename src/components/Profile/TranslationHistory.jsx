b2b5e244e408c27cd7383fb50073e7981d307b93
OzKara <oz_247@hotmail.com>
2023-09-24 20:54:29 +0200

    Fixed bug where the fetchTranslation function made dependencies of useEffect Hook change on every render

--- src/components/Profile/TranslationHistory.jsx
+++ src/components/Profile/TranslationHistory.jsx
@@ -1,52 +1,53 @@
 import React, { useEffect, useState } from "react";
 
 const TranslationHistory = () => {
-  const [translations, setTranslations] = useState([]);
-  const username = localStorage.getItem("username");
+	const [translations, setTranslations] = useState([]);
+	const username = localStorage.getItem("username");
 
-  const fetchTranslations = async () => {
-    try {
-      const response = await fetch(
-        `https://translations-api-production-3e9d.up.railway.app/translations?username=${username}`,
-        {
-          method: "GET",
-          headers: {
-            "X-API-Key":
-              "xqW942yHAcoehSs1JRI9pMbAqTNIGl0hFEIdLgvS6cgogVlCrWzn7bWMIULvxQ3o",
-            "Content-Type": "application/json",
-          },
-        }
-      );
+	useEffect(() => {
+		const fetchTranslations = async () => {
+			try {
+				const response = await fetch(
+					`https://translations-api-production-3e9d.up.railway.app/translations?username=${username}`,
+					{
+						method: "GET",
+						headers: {
+							"X-API-Key":
+								"xqW942yHAcoehSs1JRI9pMbAqTNIGl0hFEIdLgvS6cgogVlCrWzn7bWMIULvxQ3o",
+							"Content-Type": "application/json",
+						},
+					}
+				);
 
-      if (!response.ok) {
-        throw new Error("Failed to fetch translation history");
-      }
+				if (!response.ok) {
+					throw new Error("Failed to fetch translation history");
+				}
 
-      const data = await response.json();
+				const data = await response.json();
 
-      // Reverse the order of translations before setting the state
-      const lastTenTranslations = data[0]?.translations.reverse() || [];
-      setTranslations(lastTenTranslations);
-    } catch (error) {
-      console.error("Error fetching translation history:", error);
-    }
-  };
+				// Reverse the order of translations before setting the state
+				const lastTenTranslations =
+					data[0]?.translations.reverse() || [];
+				setTranslations(lastTenTranslations);
+			} catch (error) {
+				console.error("Error fetching translation history:", error);
+			}
+		};
 
-  useEffect(() => {
-    fetchTranslations();
-  }, [username, fetchTranslations]);
+		fetchTranslations();
+	}, [username]);
 
-  return (
-    <div>
-      <h3>Your Last 10 Translations</h3>
-      <ul>
-        {translations.slice(0, 10).map((translation, index) => (
-          <li key={index}>{translation}</li>
-        ))}
-      </ul>
-      {translations.length === 0 && <p>No translations available.</p>}
-    </div>
-  );
+	return (
+		<div>
+			<h3>Your Last 10 Translations</h3>
+			<ul>
+				{translations.slice(0, 10).map((translation, index) => (
+					<li key={index}>{translation}</li>
+				))}
+			</ul>
+			{translations.length === 0 && <p>No translations available.</p>}
+		</div>
+	);
 };
 
 export default TranslationHistory;
